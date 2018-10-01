811c91e2ff0d5e50f6907db0178ae4740efe1b6f
workstation-1 <workstation-1@automate-demo.com>
2018-10-01 08:20:38 +0000

    updates

--- create_your_nodes/recipes/default.rb
+++ create_your_nodes/recipes/default.rb
@@ -3,3 +3,9 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+powershell_script 'change password' do
+    code <<-EOH
+    net user Administrator "Training1234"
+    EOH
+end
